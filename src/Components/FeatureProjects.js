2ef2c7692f4750ca3db85649a056dcfa23f213bb
mattenbar <matthw.enbar@gmail.com>
2020-09-16 10:33:31 -0400

    photo and about being disp;ayed, getting started on feature carousel

---
+++ src/Components/FeatureProjects.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FeaturedProjects = () => {
+
+  return(
+    <div>
+      <p>carousel for featured projects goes here</p>
+    </div>
+  )
+
+
+}
+
+export default FeaturedProjects
--- src/Components/home.js
+++ src/Components/home.js
@@ -1,11 +1,47 @@
 import React from 'react'
+import Image from 'react-bootstrap/Image'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import FeaturedProjects from './FeatureProjects'
+
 
 const Home = () =>{
 
   
     return (
       <div>
-        hello from home
+        <Row>
+          <Col xs={6} md={4}>
+            <br></br>
+            <Image src="https://pbs.twimg.com/profile_images/1293206456280506369/gqjy9Kyp_400x400.jpg" roundedCircle />
+          </Col>
+          <Col>
+            <br/><br/><br/>
+            <div className="about-me-home-page">
+              <h1>Matthew Enbar</h1>
+              <h5>Full-Stack Developer | Offering Left Brain Software Engineering Solutions with Right Brain Creativity</h5>
+              <br />
+              <p>Experienced in Ruby on Rails and JavaScript based programming and a background in Photography.
+              <br /> 
+              Possessing strong skills in team-building, project management, and leadership that help companies solve technical problems while allowing for scalable growth.
+              <br />
+              As a photographer I led various projects and collaborated with cross functional stakeholders to discuss opportunities and offer a unique perspective to help build brand awareness, unique content and company growth.
+              <br /><br />
+              Technical Skills:
+              <br />
+              CSS, Git, HTML, JavaScript, Postgres, React, Redux, Ruby, Ruby on Rails, Sinatra, SQL. 
+              </p>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6} md={4} >
+            <br /><br /><br />
+            <div className="featured-projects-carousel-home">
+              <FeaturedProjects />
+            </div>
+          </Col>
+        </Row>
       </div>
     )
   
